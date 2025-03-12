79fc9a079ba81f6a8c6c3ce520d56e67ae87ab57
andres <anacleto115@hotmail.com>
2025-03-12 07:46:44 -0500

    Clase 12-03-2025 7:46

--- ConsoleApp1/Conexiones/ConexionET.cs
+++ ConsoleApp1/Conexiones/ConexionET.cs
@@ -1,12 +1,38 @@
+using ConsoleApp1.Modelos;
+using Microsoft.EntityFrameworkCore;
+
 namespace ConsoleApp1.Conexiones
 {
     public class ConexionEF
     {
-        public string string_conexion = "";
+        private string string_conexion = "server=localhost;database=db_clase12032025;Integrated Security=True;TrustServerCertificate=true;";
+        // server=localhost;database=db_clase12032025;uid=sa;pwd=Clas3sPrO2024_!;TrustServerCertificate=true;
+        // server=localhost;database=db_clase12032025;Integrated Security=True;TrustServerCertificate=true;
 
         public void ConexionBasica()
         {
+            var conexion = new Conexion();
+            conexion.StringConnection = this.string_conexion;
 
+            var lista = conexion.Estados.ToList();
+
+            foreach (var estado in lista)
+            {
+                Console.WriteLine(estado.Id.ToString() + ", " + estado.Nombre);
+            }
         }
+    }
+
+    public partial class Conexion : DbContext
+    {
+        public string? StringConnection { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(this.StringConnection!, p => { });
+            optionsBuilder.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTracking);
+        }
+
+        public DbSet<Estados>? Estados { get; set; }
     }
 }
--- ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net8.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33ff972d9ae03814aa2acf21bba12016f2bfbacb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e3378d043cee0b4a24628359bce99a6454bcfa2")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
